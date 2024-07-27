bcdb0f1d90d4d1df30630404a4ab39e41b0817bb
Никита <nikproserg@gmail.com>
2024-07-27 22:09:23 +0800

    add repository

---
+++ src/main/java/com/example/springsecurityjwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.example.springsecurityjwt.repository;
+
+import com.example.springsecurityjwt.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    void deleteByUserId(Long userId);
+}
